3672d20a2df8b61fff08569c8a9c39266219a57b
Richard Walker <digitalsadhu@gmail.com>
2017-08-24 17:19:41 +0200

    refactor(elder and model): refactor and move model setup to model class
    elder class now iterates through models and calls each models new setup method in order to perform
    model setup tasks

--- src/classes/elder.ts
+++ src/classes/elder.ts
@@ -24,26 +24,30 @@ export type config = {
   }
 }
 
-export type modelStore = {
+export type modelClasses = {
   [name: string]: typeof Model
 }
 
-export type typeStore = {
+export type typeClasses = {
   [name: string]: typeof Type
 }
 
-export type elderConfig = {
+export type serializerClasses = {
+  default: typeof Serializer
+  [name: string]: typeof Serializer
+}
+
+export type adapterClasses = {
+  default: typeof Adapter
+  [name: string]: typeof Adapter
+}
+
+export type options = {
   config: config
-  types?: typeStore
-  models: modelStore
-  adapters?: {
-    default: typeof Adapter
-    [name: string]: typeof Adapter
-  }
-  serializers?: {
-    default: typeof Serializer
-    [name: string]: typeof Serializer
-  }
+  types?: typeClasses
+  models: modelClasses
+  adapters?: adapterClasses
+  serializers?: serializerClasses
 }
 
 export type adapters = {
@@ -51,120 +55,192 @@ export type adapters = {
   [name: string]: Adapter
 }
 
-function setupModels(
-  models: modelStore,
-  types: typeStore,
-  adapterInstances: adapters,
-  serializerInstances: serializers
+export type serializers = {
+  default: Serializer
+  [name: string]: Serializer
+}
+
+export type types = {
+  [name: string]: Type
+}
+
+export type models = {
+  [name: string]: typeof Model
+}
+
+/**
+ * Merges a default adapter class with additional adapter classes
+ */
+function mergeAdapters(
+  defaultAdapter: typeof Adapter,
+  adapters?: {
+    [name: string]: typeof Adapter
+  }
 ) {
-  Object.keys(models).forEach(modelRef => {
-    const Model = models[modelRef]
+  return Object.assign({}, { default: defaultAdapter }, adapters)
+}
 
-    Model.adapter = adapterInstances.default
-    for (let [key, value] of Object.entries(adapterInstances)) {
-      if (key === 'default') continue
-      if (key === Model.modelName) {
-        Model.adapter = value
-      }
-    }
+/**
+ * Merges a default serializer class with additional serializer classes
+ */
+function mergeSerializers(
+  defaultSerializer: typeof Serializer,
+  serializers?: {
+    [name: string]: typeof Serializer
+  }
+) {
+  return Object.assign({}, { default: defaultSerializer }, serializers)
+}
 
-    Model.serializers = serializerInstances
+/**
+ * Merges default type classes with additional type classes
+ */
+function mergeTypes(types?: { [name: string]: typeof Type }) {
+  return Object.assign(
+    {},
+    {
+      string: StringType,
+      number: NumberType,
+      date: DateType,
+      boolean: BooleanType
+    },
+    types
+  )
+}
 
-    Object.keys(Model.meta.attributeDefinition).forEach(attr => {
-      let typeName = Model.meta.attributeDefinition[attr]
-      if (types[`${Model.modelName}:${typeName}`]) {
-        typeName = `${Model.modelName}:${typeName}`
-      }
-      const Type = types[typeName]
-      const instance = Type.create()
-      Model.meta.attributes[attr] = instance
-    })
+/**
+ * Iterates over given models calling each models setup method
+ */
+function setupModels(
+  models: modelClasses,
+  types: types,
+  adapters: adapters,
+  serializers: serializers
+) {
+  Object.keys(models).forEach(name => {
+    const Model = models[name]
+    Model.setup(types, adapters, serializers)
   })
 }
 
+/**
+ * Creates and returns adapter class instances for given adapter classes
+ */
+function setupAdapters(
+  adapters: adapterClasses,
+  config: {
+    default: databaseConfig
+    [name: string]: databaseConfig
+  }
+): adapters {
+  const instances: adapters = {
+    default: adapters.default.create(config.default)
+  }
+
+  for (let [key, value] of Object.entries(adapters)) {
+    if (key === 'default') continue
+    instances[key] = value.create(config[key])
+  }
+  return instances
+}
+
+/**
+ * Creates and returns serializer class instances for given serializer classes
+ */
+function setupSerializers(serializers: serializerClasses): serializers {
+  const instances: serializers = {
+    default: serializers.default.create()
+  }
+
+  for (let [key, value] of Object.entries(serializers)) {
+    if (key === 'default') continue
+    instances[key] = value.create()
+  }
+
+  return instances
+}
+
+/**
+ * Creates and returns type class instances for given type classes
+ */
+function setupTypes(types: typeClasses): types {
+  const instances: types = {}
+  for (let [key, value] of Object.entries(types)) {
+    instances[key] = value.create()
+  }
+  return instances
+}
+
+/**
+ * The Elder base class. Does the work of setting up and tying together
+ * models, adapters, serializers and types
+ */
 export default class Elder extends Base {
-  serializers: { [name: string]: typeof Serializer }
-  serializerInstances: serializers
-  adapters: {
-    default: typeof Adapter
-    [name: string]: typeof Adapter
-  }
-  adapterInstances: {
-    default: Adapter
-    [name: string]: Adapter
-  }
-  models: { [name: string]: typeof Model }
-  types: { [name: string]: typeof Type }
+  /**
+   * Initialized serializer classes
+   */
+  serializers: serializers
+
+  /**
+   * Initialized adapter classes
+   */
+  adapters: adapters
+
+  /**
+   * Initialized and setup model classes
+   */
+  models: models
+
+  /**
+   * Initialized type classes
+   */
+  types: types
+
+  /**
+   * Configuration reference
+   */
   config: config
 
-  static create(config: elderConfig) {
-    return new this(config)
+  /**
+   * Alternative to using `new`
+   */
+  static create(options: options) {
+    return new this(options)
   }
 
-  constructor(config: elderConfig) {
+  constructor(options: options) {
     super()
 
-    this.serializers = Object.assign(
-      {},
-      {
-        default: Serializer
-      },
-      config.serializers
-    )
+    this.config = options.config
 
-    this.serializerInstances = {
-      default: this.serializers.default.create()
-    }
+    // setup serializers by merging default classes with those optionally given in
+    // options before creating instances from classes and holding onto a reference
+    const serializers = mergeSerializers(Serializer, options.serializers)
+    this.serializers = setupSerializers(serializers)
 
-    for (let [key, value] of Object.entries(this.serializers)) {
-      if (key === 'default') continue
-      this.serializerInstances[key] = value.create()
-    }
+    // setup adapters by merging default classes with those optionally given in
+    // options before creating instances from classes and holding onto a reference
+    const adapters = mergeAdapters(PostgresAdapter, options.adapters)
+    this.adapters = setupAdapters(adapters, options.config.adapters)
 
-    this.adapters = Object.assign(
-      {},
-      {
-        default: PostgresAdapter
-      },
-      config.adapters
-    )
+    // setup types by merging default classes with those optionally given in
+    // options before creating instances from classes and holding onto a reference
+    const types = mergeTypes(options.types)
+    this.types = setupTypes(types)
 
-    this.adapterInstances = {
-      default: this.adapters.default.create(config.config.adapters.default)
-    }
-
-    for (let [key, value] of Object.entries(this.adapters)) {
-      if (key === 'default') continue
-      this.adapterInstances[key] = value.create(config.config.adapters[key])
-    }
-
-    this.types = Object.assign(
-      {},
-      {
-        string: StringType,
-        number: NumberType,
-        date: DateType,
-        boolean: BooleanType
-      },
-      config.types
-    )
-
-    this.models = config.models || {}
-
-    this.config = config.config
-
-    setupModels(
-      this.models,
-      this.types,
-      this.adapterInstances,
-      this.serializerInstances
-    )
+    // setup models by calling each models setup method passing initialized
+    // serializers, adapters and types
+    this.models = options.models || {}
+    setupModels(this.models, this.types, this.adapters, this.serializers)
   }
 
+  /**
+   * Tears down connection pools allowing scripts to exit.
+   */
   destroy(): any {
     return Promise.all(
-      Object.entries(this.adapterInstances).map(([key, value]) => {
-        return this.adapterInstances[key].destroy()
+      Object.entries(this.adapters).map(([key, value]) => {
+        return this.adapters[key].destroy()
       })
     )
   }
