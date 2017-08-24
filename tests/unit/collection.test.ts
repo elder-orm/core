c6460cc44bf5a55f1238c249dba1dd06f955887f
Richard Walker <digitalsadhu@gmail.com>
2017-08-24 17:21:20 +0200

    test(collection): Add additional test for new create method

--- tests/unit/collection.test.ts
+++ tests/unit/collection.test.ts
@@ -2,7 +2,19 @@ import Elder, { Serializer, Model, type, Collection } from '../../src'
 const config = { adapters: { default: { database: 'ash' } } }
 
 describe('Collection', () => {
-  test('Collection class serialize() method', () => {
+  test('converting to pojo', () => {
+    const models = [{ fizz: 'buzz' }, { foo: 'bar' }]
+    const collection = new Collection(...models)
+    const json = JSON.parse(JSON.stringify(collection))
+    expect(json).toEqual(models)
+  })
+
+  test('create(...item) method', () => {
+    const collection = Collection.create({ fizz: 'buzz' }, { foo: 'bar' })
+    expect(collection).toEqual([{ fizz: 'buzz' }, { foo: 'bar' }])
+  })
+
+  test('serialize() model collection', () => {
     class Cat extends Model {
       @type('string') fizz: string
     }
