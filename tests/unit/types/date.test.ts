e0fefde8b3612c885091aecb6dfaca22a4f71864
Richard Walker <digitalsadhu@gmail.com>
2017-08-28 22:06:10 +0200

    feat(types): Implement and test basic date type
    Limited to ISO8601 extended format for now. Could be extended and improved in future.

---
+++ tests/unit/types/date.test.ts
@@ -0,0 +1,46 @@
+import DateType from '../../../src/types/date'
+import { Model } from '../../../src'
+
+describe('Date type', () => {
+  test('access()', () => {
+    const dateType = DateType.create()
+    const value = new Date()
+    const result = dateType.access(value)
+    expect(result).toBe(value)
+  })
+
+  test('modify() - date object', () => {
+    const dateType = DateType.create()
+    const value = new Date()
+    const result = dateType.modify(value)
+    expect(result).toBe(value)
+  })
+
+  test('modify() - date string', () => {
+    const dateType = DateType.create()
+    const value = new Date().toISOString()
+    const result = dateType.modify(value)
+    expect(result.toISOString()).toBe(value)
+  })
+
+  test('modify() - invalid date string', () => {
+    const dateType = DateType.create()
+    const value = 'asdasdas'
+    const result = () => dateType.modify(value)
+    expect(result).toThrow()
+  })
+
+  test('store()', () => {
+    const dateType = DateType.create()
+    const value = new Date()
+    const result = dateType.store(value)
+    expect(result).toBe(value.toISOString())
+  })
+
+  test('retrieve()', () => {
+    const dateType = DateType.create()
+    const value = new Date().toISOString()
+    const result = dateType.retrieve(value)
+    expect(result.toISOString()).toBe(value)
+  })
+})
