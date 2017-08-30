f241016cb569d1bd8b354b9a2fcb0478d0d25fd4
Richard Walker <digitalsadhu@gmail.com>
2017-08-30 20:31:18 +0200

    build(test): Attempt to fix flakey tests on CI

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
-module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER
-    },
-    migrations: {
-      tableName: 'migrations'
-    }
+const config = {
+  client: 'postgresql',
+  connection: {
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER
+  },
+  migrations: {
+    tableName: 'migrations'
   }
+}
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config
 }
