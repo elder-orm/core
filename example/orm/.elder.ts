de33f7bbc567b8af15ef0bc1ac61850a3cd283f7
Richard Walker <digitalsadhu@gmail.com>
2017-09-10 21:19:37 +0200

    fix(Example): Fix incorrect database config in example project
    db values were pointing at local setup so passing locally but failing on CI

--- example/orm/.elder.ts
+++ example/orm/.elder.ts
@@ -1,12 +1,9 @@
-const config = {
-  adapters: {
-    default: {
-      database: 'ash'
-    },
-    cat: {
-      database: 'ash'
-    }
-  }
-}
+const DB_NAME: string = process.env.DB_NAME || ''
+const DB_USER: string = process.env.DB_USER || ''
 
-export default config
+export default Object.freeze({
+  adapters: {
+    default: { database: DB_NAME, user: DB_USER },
+    cat: { database: DB_NAME, user: DB_USER }
+  }
+})
